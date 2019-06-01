4cf3f8d0a0ad6fa38d109adea8cbfde039e73b93
gallopelado <juanftp100@gmail.com>
2019-06-01 15:41:36 -0400

    Definicion de modelo nacionalidad

---
+++ app/Models/NacionalidadModel.py
@@ -0,0 +1,12 @@
+from app.Conexion.Conexion import Conexion
+
+class NacionalidadModel():
+    
+    def __init__(self):
+        print('Dos hermanas, un caballo y un enano.')
+
+    def listarTodos(self):
+        pass
+    
+    def otraFuncion(self):
+        pass
