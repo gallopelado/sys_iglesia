8761dc35f589a0152a29206daf1ca37f654d548b
gallopelado <juanftp100@gmail.com>
2019-09-14 09:06:50 -0400

    get_postulacion vista sql terminado

--- app/codigos SQL/funciones/registrar_postulacion/get_postulacion_id.sql
+++ app/codigos SQL/funciones/registrar_postulacion/get_postulacion_id.sql
@@ -1,44 +1,50 @@
 CREATE OR REPLACE FUNCTION membresia.get_postulacion_id(idpostulacion integer)
-RETURNS [tipo] AS
+RETURNS TABLE(
+	idpostu integer
+	, idministerio integer
+	, ministerio varchar
+	, descripcion text
+	, documento text
+	, detalle json[]
+) AS
 $$
 /**
 * Procedimiento:get_postulacion_id
-* Parametros:
-* Descripcion:
+* Parametros: idpostulacion integer
+* Descripcion: Obtiene la postulacion registrada segun idpostulacion.
 * Autor: Juan José González Ramírez <juanftp100@gmail.com>
 * Version: 1.0
 * Fecha: 08-09-2019
 */
 BEGIN
 
-	SELECT 
-	-- Muestra datos de cabecera.
-	cab.post_id idpostulacion
-	, cab.min_id idministerio
-	, m.min_des ministerio
-	, cab.post_des descripcion
-	, cab.post_doc documento
-	-- Ver el detalle.
-	, ARRAY(
-		SELECT 
-			json_build_object(
-			'post_id',post_id, 'pro_des', pro_des
-			,'pro_id',pro_id)
-		FROM
-			membresia.postu_detalle
-		LEFT JOIN referenciales.profesiones using (pro_id)
-		WHERE
-			post_id = cab.post_id
-	) detalle
-FROM
-	membresia.cabe_postulacion cab
-LEFT JOIN 
-	referenciales.ministerios m using(min_id)
-WHERE
-	cab.post_id = 1;
+	RETURN QUERY SELECT 
+		-- Muestra datos de cabecera.
+		cab.post_id idpostulacion
+		, cab.min_id idministerio
+		, m.min_des ministerio
+		, cab.post_des descripcion
+		, cab.post_doc documento
+		-- Ver el detalle.
+		, ARRAY(
+			SELECT 
+				json_build_object(
+				'post_id',post_id, 'pro_des', pro_des
+				,'pro_id',pro_id)
+			FROM
+				membresia.postu_detalle
+			LEFT JOIN referenciales.profesiones using (pro_id)
+			WHERE
+				post_id = cab.post_id
+		) detalle
+	FROM
+		membresia.cabe_postulacion cab
+	LEFT JOIN 
+		referenciales.ministerios m using(min_id)
+	WHERE
+		cab.post_id = idpostulacion;
 
+	RETURN;	
 	
-	
-
-END
+END;
 $$ LANGUAGE plpgsql;
