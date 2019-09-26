3e7037ecefdcc8c2254d501b75a4d98a0cc4f397
gallopelado <juanftp100@gmail.com>
2019-09-26 18:11:28 -0400

    ruta para documentos de postulacion

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 # Definimos rutas estaticas.
 app.config['FORM_ADICIONAL_IMAGENES'] = RUTA_IMAGENES_FORM_ADICIONALES
 app.config['FORM_DOCUMENTOS_ARCHIVOS'] = RUTA_ARCHIVOS_FORM_DOCUMENTOS
+app.config['DOCUMENTOS_POSTULACION'] = RUTA_ARCHIVOS_POSTULACION
 
 # Suponiendo que el error sea el no haber añadido este paquete.
 from app import rutas
