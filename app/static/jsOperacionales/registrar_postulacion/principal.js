3fd808d2a978250c10aac0e970c425b8606c10d2
gallopelado <juanftp100@gmail.com>
2019-10-03 18:31:49 -0400

    se agregó evento al boton del modal personalizar

--- app/static/jsOperacionales/registrar_postulacion/principal.js
+++ app/static/jsOperacionales/registrar_postulacion/principal.js
@@ -4,16 +4,47 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const pos = new Postulacion();
 
-    pos.validarFechas();
+    // Eventos
+    // Boton cambiar para el modal personalizar.
+    const btnCambiar = document.getElementById('btnCambiar');
+    btnCambiar.addEventListener('click', () => {
+
+        // Recuperar idpostulacion
+        const idpostulacion = localStorage.getItem('idpostulacion');
+
+        // Recuperar datos del modal.
+        const txtfechainicio = document.getElementById('txt_fechainicio').value;
+        const txtfechafin = document.getElementById('txt_fechafin').value;
+
+        // Validar formulario
+        if (txtfechainicio.trim() === '') {
+            alert('No debe dejar espacios vacíos!');
+            document.getElementById('txt_fechainicio').focus();            
+            return;
+        } 
+        if (txtfechafin.trim() === ''){
+            alert('No debe dejar espacios vacíos!');
+            document.getElementById('txt_fechafin').focus();
+            return;
+        }
+
+        // Enviar datos
+        personalizarFechas(idpostulacion, txtfechainicio, txtfechafin);
+            
+        // Cerrar modal.
+        $('#modalPersonalizar').modal('toggle');
+
+    });
+
     // Funciones globales
-    window.reprogramar = (opcion, idpostulacion, fechainicio = null, fechafin = null) => {
+    window.reprogramar = async (opcion, idpostulacion, fechainicio = null, fechafin = null) => {
         let resp;
         switch (opcion) {
 
             case 'dia':
                 resp = confirm('Desea agregar 1 día ?');
                 if (resp) {
-                    let resp = pos.reprogramar(opcion, idpostulacion);
+                    let resp = await pos.reprogramar(opcion, idpostulacion);
                     if (resp.estado === true) {
                         location.href = '/membresia/formulario_postulacion';
                     } else {
@@ -25,7 +56,7 @@ document.addEventListener('DOMContentLoaded', () => {
             case 'semana':
                 resp = confirm('Desea agregar 1 semana ?');
                 if (resp) {
-                    let resp = pos.reprogramar(opcion, idpostulacion);
+                    let resp = await pos.reprogramar(opcion, idpostulacion);
                     if (resp.estado === true) {
                         location.href = '/membresia/formulario_postulacion';
                     } else {
@@ -37,7 +68,7 @@ document.addEventListener('DOMContentLoaded', () => {
             case 'mes':
                 resp = confirm('Desea agregar 1 mes ?');
                 if (resp) {
-                    let resp = pos.reprogramar(opcion, idpostulacion);
+                    let resp = await pos.reprogramar(opcion, idpostulacion);
                     if (resp.estado === true) {
                         location.href = '/membresia/formulario_postulacion';
                     } else {
@@ -47,12 +78,34 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
                 break;
             case 'personalizado':
-                alert('Abriendo modal');
+                //pos.validarFechas( '03-10-2019', '03-01-2019' );
+                pos.fechainicio.value = '';
+                pos.fechafin.value = '';
+                pos.fechainicio.focus();
+                // Iniciar el modal.
+                pos.inicializarFechas();
+                $('#modalPersonalizar').modal();
+                // Enviar el idpostulacion.
+                localStorage.setItem('idpostulacion', idpostulacion);
+                // Recolectar las fechas.
+                // Enviar datos al servidor.
                 break;
 
         }
 
     }
 
+    window.personalizarFechas = async (idpostulacion, fechainicio, fechafin) => {
+
+        try {
+
+            const res = await pos.configurarFechas(idpostulacion, fechainicio, fechafin);
+            console.log(res);
+
+        } catch (error) {
+            console.error(error);
+        }
+
+    }
 });
 
