bbda3f7d51896e63eb4f2f5cfcb1f4598d210ceb
gallopelado <juanftp100@gmail.com>
2019-10-09 18:33:51 -0300

    nuevas definiciones para la ruta

--- app/rutas/registrar_candidatos/registrar_candidatos_routes.py
+++ app/rutas/registrar_candidatos/registrar_candidatos_routes.py
@@ -1,8 +1,14 @@
 # Se importan las librerias basicas
 from flask import Blueprint, render_template, request, redirect, url_for, flash, jsonify
 
+# Se importa el modelo.
+from app.Models.ListaCandidatoModel import ListaCandidatoModel
+
 # Registrar Blueprint
 candi = Blueprint('registrar_candidatos', __name__, template_folder='templates')
+
+# Generar una instancia del modelo
+candim = ListaCandidatoModel()
 
 @candi.route('/')
 def index_candidados():
@@ -10,5 +16,14 @@ def index_candidados():
     return render_template('registrar_candidatos/index.html', titulo=titulo)
 
 
+@candi.route('/asignar_candidatos/<int:idpostulacion>', methods=['GET'])
+def asignarCandidatos(idpostulacion):
+    titulo = 'Formulario lista de candidatos'
+    return render_template('registrar_candidatos/formulario_candidatos.html', titulo=titulo, idpostulacion=idpostulacion)
+
+
 # Rutas para AJAX
-#@candi.route('/traer_postulaciones/<>')
+@candi.route('/traer_postulaciones/<string:opcion>', methods=['GET'])
+def traerPostulaciones(opcion):
+    lista = candim.traerPostulaciones(opcion)
+    return jsonify(lista)
