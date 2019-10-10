d78b42cb86691f1c8f4ef106bcf066c48d4e244a
gallopelado <juanftp100@gmail.com>
2019-10-10 18:14:35 -0300

    nuevas rutas

--- app/rutas/registrar_candidatos/registrar_candidatos_routes.py
+++ app/rutas/registrar_candidatos/registrar_candidatos_routes.py
@@ -3,6 +3,7 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash,
 
 # Se importa el modelo.
 from app.Models.ListaCandidatoModel import ListaCandidatoModel
+from app.Models.MiembroPerfilModel import MiembroPerfilModel
 
 # Registrar Blueprint
 candi = Blueprint('registrar_candidatos', __name__, template_folder='templates')
@@ -11,7 +12,7 @@ candi = Blueprint('registrar_candidatos', __name__, template_folder='templates')
 candim = ListaCandidatoModel()
 
 @candi.route('/')
-def index_candidados():
+def index_candidatos():
     titulo = 'Registrar lista de candidatos por Postulación'
     return render_template('registrar_candidatos/index.html', titulo=titulo)
 
@@ -19,11 +20,26 @@ def index_candidados():
 @candi.route('/asignar_candidatos/<int:idpostulacion>', methods=['GET'])
 def asignarCandidatos(idpostulacion):
     titulo = 'Formulario lista de candidatos'
-    return render_template('registrar_candidatos/formulario_candidatos.html', titulo=titulo, idpostulacion=idpostulacion)
+    lista = candim.traerListaCandidatos(idpostulacion)
+    detalle = lista[4]    
+    return render_template('registrar_candidatos/formulario_candidatos.html', titulo=titulo, idpostulacion=idpostulacion, lista=lista, detalle=detalle)
 
 
 # Rutas para AJAX
 @candi.route('/traer_postulaciones/<string:opcion>', methods=['GET'])
 def traerPostulaciones(opcion):
     lista = candim.traerPostulaciones(opcion)
+    return jsonify(lista)
+
+
+@candi.route('/traer_detalle_candidatos/<string:idpostulacion>')
+def traerDetalleCandidatos(idpostulacion):
+    detalle = candim.traerDetalleCandidatos(idpostulacion)
+    return jsonify(detalle)
+
+@candi.route('/traer_candidatos')
+def traerCandidatos():
+
+    perf = MiembroPerfilModel()
+    lista = perf.traerCandidatos()
     return jsonify(lista)
