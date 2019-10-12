24b6f2ee976a31f42f96727706271d5707c246a4
gallopelado <juanftp100@gmail.com>
2019-10-12 13:36:07 -0300

    nuevos eventos agregados

--- app/static/jsOperacionales/registrar_candidatos/formulario.js
+++ app/static/jsOperacionales/registrar_candidatos/formulario.js
@@ -1,20 +1,40 @@
 import ListaCandidato from './ListaCandidato.js';
 
-document.addEventListener('DOMContentLoaded', async() => {
+document.addEventListener('DOMContentLoaded', async () => {
 
     const candi = new ListaCandidato();
-    const detalle_candi = await candi.traerDetalle();
-    candi.cargarCandidatos();    
-    candi.verificaRepetidos();
+    await candi.traerDetalle();
+    await candi.cargarCandidatos();
+    //candi.verificaRepetidos();
 
     // Eventos
-    candi.txt_candidato.addEventListener('keypress', (e) => {
+    if (candi.txt_candidato !== null) {
+        candi.txt_candidato.addEventListener('keypress', (e) => {
 
-        if (e.key == 'Enter') {
-            candi.agregarFila();
-        }        
+            if (e.key == 'Enter' && candi.txt_candidato.value.trim() !== '') {
+                candi.agregarFila();
+                candi.txt_candidato.value = '';
+            }
 
-    });
+        });
+    }
+
+    if (candi.btnGuardar !== null) {
+        candi.btnGuardar.addEventListener('click', async () => {
+
+            const res = await candi.guardarLista();
+            if (res.estado === true) {
+                alert('Se guardó con éxito');
+                location.href = '/membresia/registrar_candidatos/';
+            } else if (res.tipo === 'sin-candidato'){
+                console.log(res);                
+            } else {
+                console.log(res);
+                alert('Hubo un problema al intentar guardar. Favor contacte con el administrador del sistema');
+            }
+
+        });
+    }
 
     // Funciones globales
     window.eliminarFila = (b) => {
@@ -22,14 +42,24 @@ document.addEventListener('DOMContentLoaded', async() => {
         if (conf)
             candi.tabla.deleteRow(b.rowIndex);
 
-    } 
+    }
 
 
-    window.eliminarRegistro = (idpostulacion, idcandidato) => {
+    window.eliminarRegistro = async (idpostulacion, idcandidato) => {
         const cf = confirm('Desea eliminar este registro ?');
         if (cf) {
 
-            candi.eliminarCandidato(idpostulacion, idcandidato);
+            try {
+
+                const res = await candi.eliminarCandidato(idpostulacion, idcandidato);
+
+                if (res.estado === true) {
+                    location.href = `/membresia/registrar_candidatos/asignar_candidatos/${idpostulacion}`;
+                }
+
+            } catch (error) {
+                console.error(error);
+            }
 
         }
     }
