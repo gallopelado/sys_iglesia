69e2ad000dd6c48ef41f3429c0143cf1cae30716
gallopelado <juanftp100@gmail.com>
2019-10-21 14:04:31 -0300

    definición de post para guardar formulario

--- app/rutas/registrar_comite/registrar_comite_routes.py
+++ app/rutas/registrar_comite/registrar_comite_routes.py
@@ -1,16 +1,35 @@
 # Se importan las librerías básicas
 from flask import Blueprint, render_template, request, redirect, url_for, flash, jsonify
 
+# Importar clase del formulario
+from app.rutas.registrar_comite.formularioComite import FormularioComite
+
 # Importar modelo
 
 # Registrar en Blueprint
 comi = Blueprint('registrar_comite', __name__, template_folder='templates')
+titulo = 'Registrar Comité'
 
 # Rutas
 @comi.route('/')
 def index_comite():
-    return render_template('registrar_comite/index.html')
+    return render_template('registrar_comite/index.html', titulo=titulo)
 
 @comi.route('/formulario', methods=['GET'])
 def formulario():
-    return render_template('registrar_comite/formulario_comite.html')
+    titulo = 'Formulario Comité'
+    form = FormularioComite()    
+    return render_template('registrar_comite/formulario_comite.html', titulo=titulo, form=form)
+
+
+@comi.route('/formulario', methods=['POST'])
+def guardarForm():
+    titulo = 'Formulario Comité'
+    form = FormularioComite()
+    if form.validate_on_submit():
+        print(request.form)
+        next = request.args.get('next', None)
+        if next:
+            return redirect(next)
+        return redirect(url_for('index_comite'))
+    return render_template('registrar_comite/formulario_comite.html', titulo=titulo, form=form)
