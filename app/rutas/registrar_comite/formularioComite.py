e69ae826be950cace02f029f46754f12f4543c79
gallopelado <juanftp100@gmail.com>
2019-10-21 14:04:55 -0300

    (experimental) Clase para formulario

---
+++ app/rutas/registrar_comite/formularioComite.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, HiddenField, TextAreaField 
+from wtforms.validators import DataRequired, Length
+
+class FormularioComite(FlaskForm):
+    idcomite =  HiddenField( validators=[DataRequired(), Length(min=1)], id='txt_idcomite' )
+    comite = StringField('Comité', validators=[DataRequired(), Length(max=64)], id='txt_comite')
+    idlider =  HiddenField( validators=[DataRequired(), Length(min=1)], id='txt_idlider' )
+    lider = StringField('Líder', validators=[DataRequired(), Length(max=64)], id='txt_lider')
+    idsuplente =  HiddenField( id='txt_idsuplente' )
+    suplente = StringField('Suplente', id='txt_suplente', validators=[Length(max=64)])
+    descripcion = TextAreaField('Descripción', validators=[DataRequired()], id='txt_descripcion')
+    observación = TextAreaField('Observación', id='txt_observacion')  
+     
