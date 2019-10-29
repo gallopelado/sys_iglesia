59bf942f1633d68b2837feb70a35ac22190d0e80
gallopelado <juanftp100@gmail.com>
2019-10-29 18:52:19 -0300

    ruta para formulario obrero

--- app/rutas/registrar_obrero/registrar_obrero_routes.py
+++ app/rutas/registrar_obrero/registrar_obrero_routes.py
@@ -4,14 +4,29 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash,
 # Importar modelo
 from app.Models.ComiteModel import ComiteModel
 
+# Importar formularios
+from app.rutas.registrar_obrero.formularios import *
+
 # Registrar en Blueprint
 ob = Blueprint('registrar_obrero', __name__, template_folder='templates')
 titulo = 'Registrar Obreros a Comité'
-
+tituloFormObrero = 'Formulario Lista de Obreros'
+cm = ComiteModel()
 
 # Rutas
 @ob.route('/')
-def index_obrero():
-    cm = ComiteModel()
-    lista = cm.traerTodos()
+def index_obrero():    
+    lista = cm.traerTodos()       
     return render_template('registrar_obrero/index.html', titulo=titulo, lista=lista)
+
+
+@ob.route('/formulario_obrero/<int:idcomite>', methods=['GET'])
+def formularioObrero(idcomite):    
+    comite = cm.traerPorId(idcomite)
+    form = FormAgregar() 
+    # Setear formulario
+    form.idcomite.data = comite[0]
+    form.comite.data = comite[1] 
+    form.idlider.data = comite[2]
+    form.lider.data = comite[3]  
+    return render_template('registrar_obrero/formulario.html', titulo=tituloFormObrero, comite=comite, form=form)
