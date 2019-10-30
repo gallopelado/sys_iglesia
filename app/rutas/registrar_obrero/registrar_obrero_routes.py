e427c77eeb8603ee3d7f4c2220d267c1eeed7dc1
gallopelado <juanftp100@gmail.com>
2019-10-30 18:49:36 -0300

    nuevas rutas

--- app/rutas/registrar_obrero/registrar_obrero_routes.py
+++ app/rutas/registrar_obrero/registrar_obrero_routes.py
@@ -3,15 +3,17 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash,
 
 # Importar modelo
 from app.Models.ComiteModel import ComiteModel
+from app.Models.ObreroModel import ObreroModel
 
 # Importar formularios
-from app.rutas.registrar_obrero.formularios import *
+from app.rutas.registrar_obrero.formularios import FormAgregar, FormDatosObrero
 
 # Registrar en Blueprint
 ob = Blueprint('registrar_obrero', __name__, template_folder='templates')
 titulo = 'Registrar Obreros a Comité'
 tituloFormObrero = 'Formulario Lista de Obreros'
 cm = ComiteModel()
+obm = ObreroModel()
 
 # Rutas
 @ob.route('/')
@@ -28,5 +30,26 @@ def formularioObrero(idcomite):
     form.idcomite.data = comite[0]
     form.comite.data = comite[1] 
     form.idlider.data = comite[2]
-    form.lider.data = comite[3]  
-    return render_template('registrar_obrero/formulario.html', titulo=tituloFormObrero, comite=comite, form=form)
+    form.lider.data = comite[3]    
+    ListaAdmitidos = obm.traerPostulacionesProcesadasPorMinisterio(idcomite)
+    if ListaAdmitidos:
+        form.lista_admitido.choices = [ (item[0], str(f'{item[4]} - Fecha {item[2]} - Cantidad Admitidos {item[5]}')) for item in ListaAdmitidos ]
+    else:
+        form.lista_admitido.choices = [('', 'No hay nueva lista')]    
+    return render_template('registrar_obrero/formulario.html', titulo=tituloFormObrero, comite=comite, form=form, lista_admitidos=ListaAdmitidos)
+
+
+@ob.route('/formulario_datos_obrero', methods=['POST'])
+def formularioDatosObrero():
+    titulo = 'Formulario datos obrero'
+    form = FormDatosObrero()
+    formGeneral = FormAgregar()
+    idcomite = formGeneral.idcomite.data    
+    form.idpostulacion.data = formGeneral.lista_admitido.data    
+    return render_template('registrar_obrero/formulario_datos_obrero.html', form=form, titulo=titulo , idcomite=idcomite)
+
+
+# Rutas para ajax
+@ob.route('/traer_calificados/<int:idpostulacion>', methods=['GET'])
+def traerCalificados(idpostulacion):
+    return jsonify(obm.traerCalificadosPorPostulacion(idpostulacion))
