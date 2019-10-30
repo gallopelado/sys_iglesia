134d84c7ef76af4f5847d5e480e1962c741070aa
gallopelado <juanftp100@gmail.com>
2019-10-30 18:49:13 -0300

    nueva clase para el formulario obrero

--- app/rutas/registrar_obrero/formularios.py
+++ app/rutas/registrar_obrero/formularios.py
@@ -1,10 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, HiddenField, TextAreaField, SelectField 
+from wtforms import StringField, SubmitField, HiddenField, TextAreaField, SelectField, DateField, BooleanField 
 from wtforms.validators import DataRequired, Length
 
 class FormAgregar(FlaskForm):
+    idpostulacion = HiddenField(validators=[DataRequired()])
     idcomite =  HiddenField( validators=[DataRequired(), Length(min=1)], id='txt_idcomite' )
     comite = StringField('Comité', validators=[Length(max=64)], id='txt_comite')
     idlider =  HiddenField( validators=[DataRequired(), Length(min=1)], id='txt_idlider' )
     lider = StringField('Líder', validators=[DataRequired(), Length(max=64)], id='txt_lider')
-    lista_admitido = SelectField('Elegir Lista de admitidos', choices=[('cpp', 'C++'), ('py', 'Python'), ('text', 'Plain Text')])
+    lista_admitido = SelectField('Elegir Lista de admitidos')
+    agregaObrero = SubmitField('Agregar nuevo obrero')
+
+class FormDatosObrero(FlaskForm):
+    idpostulacion = HiddenField(validators=[DataRequired()])
+    idobrero = HiddenField(validators=[DataRequired(), Length(min=1)])
+    obrero = StringField('Obrero', validators=[DataRequired()])
+    fechaingreso = DateField('Fecha de ingreso', validators=[DataRequired()])
+    entrenamiento = BooleanField('En entrenamiento')
+    observacion = StringField('Observación')
+    guardar = SubmitField('Guardar')
