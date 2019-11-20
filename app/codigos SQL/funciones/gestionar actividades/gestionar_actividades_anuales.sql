a76e6f1ae0653b4437bffd145891162fa85c43f4
gallopelado <juanftp100@gmail.com>
2019-11-19 22:09:27 -0300

    definición del procedimiento para gestionar actividades

--- app/codigos SQL/funciones/gestionar actividades/gestionar_actividades_anuales.sql
+++ app/codigos SQL/funciones/gestionar actividades/gestionar_actividades_anuales.sql
@@ -10,10 +10,58 @@ create or replace procedure membresia.gestionar_actividades_anuales(
  * Autor: Juan José González Ramírez <juanftp100@gmail.com>
  * versión 1.0
 */
+DECLARE
+	v_actobs TEXT := TRIM(UPPER(actobs));
+	fecha_actual TIMESTAMP := now();	
 begin
 	CASE opcion
 		WHEN 'registrar' THEN
+			INSERT INTO actividades.actividades(
+				anho_habil
+				, eve_id
+				, lug_id
+				, act_fecha
+				, act_hora
+				, plaz_id
+				, act_repite
+				, act_obs
+				, min_id
+				, creado_por_usuario
+				, creacion_fecha
+				, modificado_por_usuario
+				, modif_fecha
+			)
+			VALUES (
+				anhohabil
+				, eveid
+				, lugid
+				, actfecha
+				, acthora
+				, plazid
+				, actrepite
+				, v_actobs
+				, minid
+				, creadoporusuario
+				, fecha_actual
+				, NULL
+				, NULL
+			);
+			raise notice 'Actividad registrada';
 		WHEN 'modificar' THEN
+			UPDATE actividades.actividades
+			SET 								
+				lug_id = lugid
+				, act_fecha=?
+				, act_hora=?
+				, plaz_id=?
+				, act_repite=?
+				, act_obs=?
+				, min_id=?
+				, creado_por_usuario=?
+				, creacion_fecha=?
+				, modificado_por_usuario=?
+				, modif_fecha=?
+			WHERE anho_habil = anhohabil AND eve_id = eveid;
 		WHEN 'eliminar' THEN
 		WHEN 'repetir_por_año' THEN
 	END CASE;
