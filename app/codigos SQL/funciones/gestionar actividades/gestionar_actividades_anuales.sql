fb0559460cafb3d5886212b0bee0792039baf7f5
gallopelado <juanftp100@gmail.com>
2019-11-25 20:02:04 -0300

    procedimiento para gestionar abm de actividades

--- app/codigos SQL/funciones/gestionar actividades/gestionar_actividades_anuales.sql
+++ app/codigos SQL/funciones/gestionar actividades/gestionar_actividades_anuales.sql
@@ -1,6 +1,7 @@
-create or replace procedure membresia.gestionar_actividades_anuales(
-	opcion varchar, anhohabil integer, eveid integer, lugid integer, actfecha date, 
-	acthora time, plazid integer, actrepite boolean, actobs text, minid integer, creadoporusuario integer
+create or replace procedure actividades.gestionar_actividades_anuales(
+	opcion varchar, id integer, anhohabil integer, eveid integer, lugid integer, 
+	fechaini date, horaini time, fechafin date, horafin time, plazid integer, 
+	actrepite boolean, actobs text, minid integer, creadoporusuario integer
 ) language plpgsql as $$
 /**
  * Procedimiento gestionar_actividades_anuales
@@ -11,59 +12,154 @@ create or replace procedure membresia.gestionar_actividades_anuales(
  * versión 1.0
 */
 DECLARE
-	v_actobs TEXT := TRIM(UPPER(actobs));
-	fecha_actual TIMESTAMP := now();	
+	v_actobs TEXT := TRIM(UPPER(actobs));	
+	fecha_actual TIMESTAMP := now();
+	v_anho integer;
+	v_lugar text;
+	actividades refcursor;
+	reg record;	
 begin
+	-- Validar año habil.
+	select anho_id into v_anho from referenciales.anho_habil where anho_des = anhohabil and is_active = TRUE;
+	if not found THEN
+		raise exception 'Este anho no esta habilitado' using ERRCODE = '20500';
+	end if;
+	--Obtener descripcion de lugar.
+	select lug_des into v_lugar from referenciales.lugares where lug_id = lugid;
+	if not found THEN
+		raise exception 'Este lugar no parece valido' using ERRCODE = '20501';
+	end if;
+
 	CASE opcion
 		WHEN 'registrar' THEN
-			INSERT INTO actividades.actividades(
-				anho_habil
-				, eve_id
-				, lug_id
-				, act_fecha
-				, act_hora
-				, plaz_id
-				, act_repite
-				, act_obs
-				, min_id
-				, creado_por_usuario
-				, creacion_fecha
-				, modificado_por_usuario
-				, modif_fecha
-			)
-			VALUES (
-				anhohabil
-				, eveid
-				, lugid
-				, actfecha
-				, acthora
-				, plazid
-				, actrepite
-				, v_actobs
-				, minid
-				, creadoporusuario
-				, fecha_actual
-				, NULL
-				, NULL
-			);
-			raise notice 'Actividad registrada';
+			if actividades.verificar_actividad(v_lugar, fechaini, horaini, fechafin, horafin) is TRUE THEN
+				INSERT INTO actividades.actividades
+				(					
+					anho_id
+					, eve_id
+					, lug_id
+					, act_fechainicio
+					, act_horainicio
+					, act_fechafin
+					, act_horafin
+					, plaz_id
+					, act_repite
+					, act_obs
+					, min_id
+					, creado_por_usuario
+					, creacion_fecha
+					, modificado_por_usuario
+					, modif_fecha
+				)
+				VALUES 
+					( 
+						v_anho
+						, eveid
+						, lugid
+						, fechaini
+						, horaini
+						, fechafin
+						, horafin
+						, plazid
+						, actrepite
+						, v_actobs
+						, minid
+						, creadoporusuario
+						, fecha_actual
+						, NULL
+						, NULL
+					);
+				raise notice 'Se inserto correctamente el registro actividad';
+			ELSE
+				raise exception 'Quiza existe una actividad en dichas fechas' using ERRCODE = '20600';
+			end if;
 		WHEN 'modificar' THEN
-			UPDATE actividades.actividades
-			SET 								
-				lug_id = lugid
-				, act_fecha=?
-				, act_hora=?
-				, plaz_id=?
-				, act_repite=?
-				, act_obs=?
-				, min_id=?
-				, creado_por_usuario=?
-				, creacion_fecha=?
-				, modificado_por_usuario=?
-				, modif_fecha=?
-			WHERE anho_habil = anhohabil AND eve_id = eveid;
+			if actividades.verificar_actividad(v_lugar, fechaini, horaini, fechafin, horafin) is TRUE THEN
+				UPDATE actividades.actividades
+				SET 
+					anho_id = v_anho
+					, eve_id = eveid
+					, lug_id = lugid
+					, act_fechainicio = fechaini
+					, act_horainicio = horaini
+					, act_fechafin = fechafin
+					, act_horafin = horafin
+					, plaz_id = plazid
+					, act_repite = actrepite
+					, act_obs = v_actobs
+					, min_id = minid
+					, modificado_por_usuario = creadoporusuario
+					, modif_fecha = fecha_actual
+				WHERE act_id = id;
+				raise notice 'Se modifico correctamente el registro actividad';
+			ELSE
+				raise exception 'Quiza existe una actividad en dichas fechas' using ERRCODE = '20600';
+			end if;
 		WHEN 'eliminar' THEN
+		-- Solo puede eliminarse registros que pertenecen al año actual, no menores al él.
+		if anhohabil >= EXTRACT(year from fecha_actual) THEN
+			delete from actividades.actividades
+			where act_id = id;
+		ELSE
+			raise exception 'No se ha podido eliminar el registro actividad' using ERRCODE = '20601';
+		end if;
 		WHEN 'repetir_por_año' THEN
+		/*
+		* Hacer la insercion en masa de la coleccion cuyo atributo act_repite es true,
+		* con la condicion de que el año actual se le sume 1. Esta opcion se realiza el ultimo 
+		* dia del año o cuando se habilite el año habil para la programacion adelantada.
+		*/
+		open actividades for select * from actividades.actividades;
+		if anhohabil = extract(year from fecha_actual) THEN
+			anhohabil := anhohabil + 1;
+			select anho_id into v_anho from referenciales.anho_habil where anho_des = anhohabil and is_active = FALSE;
+			if not found THEN
+				raise exception 'Todavía no se agregó este año = % al sistema', anhohabil;
+			end if;
+			loop
+				fetch actividades into reg;
+				exit when not found;
+				if reg.act_repite is true THEN
+					INSERT INTO actividades.actividades
+						(					
+							anho_id
+							, eve_id
+							, lug_id
+							, act_fechainicio
+							, act_horainicio
+							, act_fechafin
+							, act_horafin
+							, plaz_id
+							, act_repite
+							, act_obs
+							, min_id
+							, creado_por_usuario
+							, creacion_fecha
+							, modificado_por_usuario
+							, modif_fecha
+						)
+					VALUES 
+						( 
+							v_anho
+							, reg.eve_id
+							, reg.lug_id
+							, (select (reg.act_fechainicio + interval '1 year')::date)
+							, reg.act_horainicio
+							, (select (reg.act_fechafin + interval '1 year')::date)
+							, reg.act_horafin
+							, reg.plaz_id
+							, reg.act_repite
+							, reg.act_obs
+							, reg.min_id
+							, creadoporusuario
+							, fecha_actual
+							, NULL
+							, NULL
+						);
+				end if;
+			end loop;
+		end if;
+		close actividades;
 	END CASE;
 end
 $$;
