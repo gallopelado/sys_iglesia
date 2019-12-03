9684dc11d290942e26623c2807697d2b943df547
gallopelado <juanftp100@gmail.com>
2019-12-03 19:20:19 -0300

    instancia para obtener anho habil

--- app/rutas/gestionar_actividades/registrar_actividades_anuales/actividades_anuales_routes.py
+++ app/rutas/gestionar_actividades/registrar_actividades_anuales/actividades_anuales_routes.py
@@ -1,10 +1,14 @@
 # Se importan las librerias basicas
 from flask import Blueprint, render_template, request, redirect, url_for, flash, jsonify
 # Importar modelos
+from app.Models.actividad_models.ActividadAnualModel import ActividadAnualModel
 # Registrar módulo
 # acan -- actividad anual
 acan = Blueprint('actividades_anuales', __name__, template_folder='templates')
 titulo = 'Registrar actividades anuales de la organización'
+# Instancias
+actim = ActividadAnualModel()
 @acan.route('/')
 def index_acti_anuales():
-    return render_template('registrar_actividades_anuales/index.html', titulo=titulo)
+    lista = actim.obtenerAnho()
+    return render_template('registrar_actividades_anuales/index.html', titulo=titulo, anhos=lista)
