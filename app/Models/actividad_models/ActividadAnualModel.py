386d7998f42278ec9c4a58d55d0e2f880378a8b4
gallopelado <juanftp100@gmail.com>
2019-12-03 20:44:39 -0300

    nuevo metodo obtenerActividadesJson

--- app/Models/actividad_models/ActividadAnualModel.py
+++ app/Models/actividad_models/ActividadAnualModel.py
@@ -1,5 +1,6 @@
 from app.Conexion.Conexion import Conexion
 
+
 class ActividadAnualModel:
 
     def obtenerAnho(self):
@@ -10,6 +11,30 @@ class ActividadAnualModel:
             cur = con.cursor()
             cur.execute(consulta)
             return cur.fetchall()
+        except con.Error as e:
+            print(e.pgerror)
+        finally:
+            if con is not None:
+                cur.close()
+                con.close()
+
+    def obtenerActividadesJson(self, anho):
+        try:
+            consulta = '''
+            SELECT array_to_json(array_agg(row_to_json(datos)))
+            FROM (
+                SELECT idactividad
+                        , anho
+                        , evento
+                        , fecha_formatolargo(fechainicio)fechainicio
+                        , fecha_formatolargo(fechafin)fechafin 
+                FROM actividades.get_actividades(%s))datos;
+            '''
+            conexion = Conexion()
+            con = conexion.getConexion()
+            cur = con.cursor()
+            cur.execute(consulta, (anho,))            
+            return cur.fetchone()[0]
         except con.Error as e:
             print(e.pgerror)
         finally:
