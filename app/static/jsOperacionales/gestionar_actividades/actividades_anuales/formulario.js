bd3ea620e9de143fe5cfd4073a4366e4b5a84002
gallopelado <juanftp100@gmail.com>
2019-12-12 20:38:45 -0300

    función para verificar año en js del formulario

--- app/static/jsOperacionales/gestionar_actividades/actividades_anuales/formulario.js
+++ app/static/jsOperacionales/gestionar_actividades/actividades_anuales/formulario.js
@@ -2,7 +2,24 @@
  * Controlador del formulario
  */
 
- document.addEventListener('DOMContentLoaded', () => {
-    // Formatear los combos con Select2
-    $('select').select2();
- });
+document.addEventListener('DOMContentLoaded', () => {
+   // Formatear los combos con Select2
+   $('select').select2();
+   const alerta = `<span class="badge badge-warning">Plan futuro</span>`;
+   const anho = document.getElementById('anho');
+   const div =  document.getElementById('alerta-anho');
+
+   const verificaAnho = async (anho) => {
+      try {
+         const res = await fetch(`/actividades/registrar_actividades_anuales/verificarAnhoFuturo/${anho}`)
+         const data = await res.json();
+         if (data) {
+            div.innerHTML = alerta;
+         }
+      } catch (error) {
+         console.error(error);
+      }
+   }
+
+   verificaAnho(anho.value);
+});
