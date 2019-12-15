db1617667ae7b6e9bf94b7385862d3b93906a91a
gallopelado <juanftp100@gmail.com>
2019-12-15 20:12:50 -0300

    mensaje de confirmación y activación de modales para lugar y evento

--- app/static/jsOperacionales/gestionar_actividades/actividades_anuales/formulario.js
+++ app/static/jsOperacionales/gestionar_actividades/actividades_anuales/formulario.js
@@ -1,3 +1,4 @@
+import { mensajeConfirmacion } from '../../helper/helper.js';
 /**
  * Controlador del formulario
  */
@@ -7,7 +8,25 @@ document.addEventListener('DOMContentLoaded', () => {
    $('select').select2();
    const alerta = `<span class="badge badge-warning">Plan futuro</span>`;
    const anho = document.getElementById('anho');
-   const div =  document.getElementById('alerta-anho');
+   const div = document.getElementById('alerta-anho');
+   const btnEvento = document.getElementById('btAgregaEvento');
+   const btnLugar = document.getElementById('btAgregaLugar');
+
+   //Agregar eventos a los botones
+   btnEvento.addEventListener('click', () => {
+      const mc = mensajeConfirmacion('Confirmar', 'Desea agregar un evento ?');
+      mc.buttons.Si.action = () => {
+         $('#modalEvento').modal();
+      }
+      mc.open();
+   });
+   btnLugar.addEventListener('click', () => {
+      const mc = mensajeConfirmacion('Confirmar', 'Desea agregar un lugar ?');
+      mc.buttons.Si.action = () => {
+         $('#modalLugar').modal();
+      }
+      mc.open();
+   });
 
    const verificaAnho = async (anho) => {
       try {
