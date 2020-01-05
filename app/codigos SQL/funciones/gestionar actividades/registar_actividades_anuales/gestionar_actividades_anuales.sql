bb74e0501c952a29295965eef64b3fc52b76941b
gallopelado <juanftp100@gmail.com>
2020-01-05 17:29:36 -0300

    validación para fechas que se repiten en años futuros

--- app/codigos SQL/funciones/gestionar actividades/registar_actividades_anuales/gestionar_actividades_anuales.sql
+++ app/codigos SQL/funciones/gestionar actividades/registar_actividades_anuales/gestionar_actividades_anuales.sql
@@ -9,7 +9,7 @@ create or replace procedure actividades.gestionar_actividades_anuales(
  * con opciones como:
  * registrar, modificar, eliminar, repetir_por_año
  * Autor: Juan José González Ramírez <juanftp100@gmail.com>
- * versión 1.2
+ * versión 1.4
 */
 DECLARE
 	v_actobs TEXT := TRIM(UPPER(actobs));	
@@ -24,13 +24,15 @@ DECLARE
 	v_horafin time;
 	
 begin
-	-- Validar año habil.
-	select anho_id into v_anho from referenciales.anho_habil where anho_des = anhohabil and is_active = TRUE;
-	if not found THEN
-		raise exception 'Este año no esta habilitado' using ERRCODE = '20500';
+	if opcion != 'repetir_por_año' then
+		-- Validar año habil.
+		select anho_id into v_anho from referenciales.anho_habil where anho_des = anhohabil and is_active = TRUE;
+		if not found THEN
+			raise exception 'Este año no esta habilitado' using ERRCODE = '20500';
+		end if;
 	end if;
 	
-	if opcion = 'registrar' or opcion = 'modificar' then
+	if opcion IN ('registrar', 'modificar') then
 		--Obtener descripcion de lugar.
 		select lug_des into v_lugar from referenciales.lugares where lug_id = lugid;
 		if not found THEN
@@ -130,70 +132,70 @@ begin
 				end if;
 			end if;
 		WHEN 'eliminar' THEN
-		-- Solo puede eliminarse registros que pertenecen al año actual, no menores al él.
-		if anhohabil >= EXTRACT(year from fecha_actual) THEN
-			delete from actividades.actividades
-			where act_id = id;
-		ELSE
-			raise exception 'No se ha podido eliminar el registro actividad' using ERRCODE = '20601';
-		end if;
-		WHEN 'repetir_por_año' THEN
-		/*
-		* Hacer la insercion en masa de la coleccion cuyo atributo act_repite es true,
-		* con la condicion de que el año actual se le sume 1. Esta opcion se realiza el ultimo 
-		* dia del año o cuando se habilite el año habil para la programacion adelantada.
-		*/
-		open actividades for select * from actividades.actividades;
-		if anhohabil = extract(year from fecha_actual) THEN
-			anhohabil := anhohabil + 1;
-			select anho_id into v_anho from referenciales.anho_habil where anho_des = anhohabil and is_active = FALSE;
-			if not found THEN
-				raise exception 'Todavía no se agregó este año = % al sistema', anhohabil;
+			-- Solo puede eliminarse registros que pertenecen al año actual, no menores al él.
+			if anhohabil >= EXTRACT(year from fecha_actual) THEN
+				delete from actividades.actividades
+				where act_id = id;
+			ELSE
+				raise exception 'No se ha podido eliminar el registro actividad' using ERRCODE = '20601';
 			end if;
-			loop
-				fetch actividades into reg;
-				exit when not found;
-				if reg.act_repite is true THEN
-					INSERT INTO actividades.actividades
-						(					
-							anho_id
-							, eve_id
-							, lug_id
-							, act_fechainicio
-							, act_horainicio
-							, act_fechafin
-							, act_horafin
-							, plaz_id
-							, act_repite
-							, act_obs
-							, min_id
-							, creado_por_usuario
-							, creacion_fecha
-							, modificado_por_usuario
-							, modif_fecha
-						)
-					VALUES 
-						( 
-							v_anho
-							, reg.eve_id
-							, reg.lug_id
-							, (select (reg.act_fechainicio + interval '1 year')::date)
-							, reg.act_horainicio
-							, (select (reg.act_fechafin + interval '1 year')::date)
-							, reg.act_horafin
-							, reg.plaz_id
-							, reg.act_repite
-							, reg.act_obs
-							, reg.min_id
-							, creadoporusuario
-							, fecha_actual
-							, NULL
-							, NULL
-						);
+		WHEN 'repetir_por_año' THEN
+			/*
+			* Hacer la insercion en masa de la coleccion cuyo atributo act_repite es true,
+			* con la condicion de que el año actual se le sume 1. Esta opcion se realiza el ultimo 
+			* dia del año o cuando se habilite el año habil para la programacion adelantada.
+			*/
+			open actividades for select * from actividades.actividades;
+			if anhohabil = extract(year from fecha_actual) THEN
+				anhohabil := anhohabil + 1;
+				select anho_id into v_anho from referenciales.anho_habil where anho_des = anhohabil and is_active = FALSE and adelantar = TRUE;
+				if not found THEN
+					raise exception 'Todavía no se agregó este año = % al sistema', anhohabil;
 				end if;
-			end loop;
-		end if;
-		close actividades;
+				loop
+					fetch actividades into reg;
+					exit when not found;
+					if reg.act_repite is true THEN
+						INSERT INTO actividades.actividades
+							(					
+								anho_id
+								, eve_id
+								, lug_id
+								, act_fechainicio
+								, act_horainicio
+								, act_fechafin
+								, act_horafin
+								, plaz_id
+								, act_repite
+								, act_obs
+								, min_id
+								, creado_por_usuario
+								, creacion_fecha
+								, modificado_por_usuario
+								, modif_fecha
+							)
+						VALUES 
+							( 
+								v_anho
+								, reg.eve_id
+								, reg.lug_id
+								, (select (reg.act_fechainicio + interval '1 year')::date)
+								, reg.act_horainicio
+								, (select (reg.act_fechafin + interval '1 year')::date)
+								, reg.act_horafin
+								, reg.plaz_id
+								, reg.act_repite
+								, reg.act_obs
+								, reg.min_id
+								, creadoporusuario
+								, fecha_actual
+								, NULL
+								, NULL
+							);
+					end if;
+				end loop;
+			end if;
+			close actividades;
 	END CASE;
 end
 $$;
