66c107db3d8c00095b71c50991632b96ccc34f8c
Juan <juanftp100@gmail.com>
2020-01-05 22:41:48 -0300

    ruta para llamar al formulario de asistencia

--- app/rutas/gestionar_actividades/registrar_asistencia/registrar_asistencia_routes.py
+++ app/rutas/gestionar_actividades/registrar_asistencia/registrar_asistencia_routes.py
@@ -4,8 +4,13 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash,
 
 # Registrar modulo
 asis = Blueprint('registrar_asistencia', __name__, template_folder='templates')
-titulo = 'Registrar asistencias del miembros de la iglesia'
+titulo = 'Registrar asistencias de miembros de la iglesia'
 
 @asis.route('/')
 def index_asistencia():
     return render_template('registrar_asistencia/index.html', titulo=titulo)
+
+
+@asis.route('/formulario_asistencia')
+def formularioAsistencia():
+    return render_template('registrar_asistencia/formulario.html', titulo='Formulario Asistencia')
