4dffaf04d671c8a6e3c82bb8d9f100fcf4185b9e
Juan <juanftp100@gmail.com>
2020-01-19 19:55:28 -0300

    Formulario de Contrato en construcción al 40%

---
+++ app/rutas/gestionar_actividades/generar_contrato_reserva/formularios.py
@@ -0,0 +1,34 @@
+from datetime import date,time, datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, HiddenField, TextAreaField, SelectField, BooleanField 
+from wtforms.fields.html5 import DateField, TimeField
+from wtforms.validators import DataRequired, Length
+from app.Models.ReferencialModel import ReferencialModel
+
+class Formulario(FlaskForm):
+    refm = ReferencialModel()    
+    lista_eventos = refm.getAll('referenciales.eventos')
+    lista_ministerios = refm.getAll('referenciales.ministerios')
+    lista_lugares = refm.getAll('referenciales.lugares')
+    lista_plazos = refm.getAll('referenciales.plazos')
+
+    anho = StringField('Año hábil')
+    evento = SelectField('Elegir Evento', validators=[DataRequired()], choices=[(item[0], item[1]) for item in lista_eventos], coerce=int)
+    comite = SelectField('Organiza', validators=[DataRequired()], choices=[(item[0], item[1]) for item in lista_ministerios], coerce=int)
+    lugar = SelectField('Lugar', validators=[DataRequired()], choices=[(item[0], item[1]) for item in lista_lugares], coerce=int)
+    fechainicio = DateField('Fecha inicio', validators=[DataRequired()], format='%Y-%m-%d', default=date.today)
+    horainicio = TimeField('Hora inicio', validators=[DataRequired()], format='%H:%M', default=datetime.now())
+    fechafin = DateField('Fecha fin', validators=[DataRequired()], format='%Y-%m-%d', default=date.today)
+    horafin = TimeField('Hora fin', validators=[DataRequired()], format='%H:%M',default=datetime.now())
+    plazo = SelectField('Plazo', validators=[DataRequired()], choices=[(item[0], item[1]) for item in lista_plazos], coerce=int)
+    repite = BooleanField('Se repite cada año ?')
+    obs = TextAreaField('Observación')
+    registrar = SubmitField('Registrar')
+
+    def __init__(self, *args, **kwargs):
+        super(Formulario, self).__init__(*args, **kwargs)        
+        self.evento.choices = self.refm.getAll('referenciales.eventos')
+        self.comite.choices  = self.refm.getAll('referenciales.ministerios')
+        self.lugar.choices =  self.refm.getAll('referenciales.lugares')
+        self.plazo.choices = self.refm.getAll('referenciales.plazos')
+
--- app/rutas/gestionar_actividades/generar_contrato_reserva/generar_contrato_reserva_routes.py
+++ app/rutas/gestionar_actividades/generar_contrato_reserva/generar_contrato_reserva_routes.py
@@ -6,6 +6,13 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash,
 # gcr - generar contrato reserva
 gcr = Blueprint('contrato_reserva', __name__, template_folder='templates')
 titulo = 'Generar contrato por reserva'
+
 @gcr.route('/')
 def index_contrato():
     return render_template('contrato_reserva/index.html', titulo=titulo)
+
+
+@gcr.route('/formulario_contrato')
+def formularioContrato():
+    titulo = 'Formulario contrato'
+    return render_template('contrato_reserva/formulario.html', titulo=titulo)
