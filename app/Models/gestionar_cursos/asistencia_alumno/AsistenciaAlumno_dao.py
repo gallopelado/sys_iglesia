64b20ed063fc4cc95cc718e93c26cd0b85d3443c
Juan <juanftp100@gmail.com>
2020-08-11 21:38:08 -0400

    fix: mostrar fechas de clase segun mes actual

--- app/Models/gestionar_cursos/asistencia_alumno/AsistenciaAlumno_dao.py
+++ app/Models/gestionar_cursos/asistencia_alumno/AsistenciaAlumno_dao.py
@@ -9,7 +9,7 @@ class AsistenciaAlumno_dao(Conexion):
         res = {}
         lista = []
         querySQL = '''SELECT malla_id, cur_id, asi_id, num_id, per_id, cur_des, asi_des, num_des, dia, fecha::VARCHAR, fecha_formatolargo(fecha)fecha_larga 
-        FROM cursos.lista_cursos_fecha(%s, %s, %s, %s, %s, %s) ORDER BY fecha ASC'''
+        FROM cursos.lista_cursos_fecha(%s, %s, %s, %s, %s, %s) WHERE EXTRACT(MONTH FROM fecha) = EXTRACT(MONTH FROM CURRENT_DATE) ORDER BY fecha ASC'''
         try:
             conn = self.getConexion()
             cur = conn.cursor()
